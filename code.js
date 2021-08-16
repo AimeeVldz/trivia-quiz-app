0d0175c31b6ab9c9b44a63a2030dccd33b6b3140
AimeeVldz <aimeevldz@gmail.com>
2021-08-16 08:20:50 -0500

    Test

--- code.js
+++ code.js
@@ -1,4 +1,69 @@
-// To run this assignment, right click on index.html in the Visual Studio Code file explorer to the left
-// and select "Open with Live Server"
+let allClues = [];
+let score = 0;
 
-// YOUR CODE HERE!
+let clueDisplay = document.querySelector(".clue-text");
+let correctAnswer = document.querySelector(".correct-answer");
+let answerInput = document.getElementById("user-answer");
+let scoreDisplay = document.querySelector(".score-count")
+let answerValue = answerInput.value;
+let question = ""
+let answer = ""
+function fetchingData() {
+  fetch(`https://jservice.io/api/random?id=${Math.floor(Math.random())}count=100`)
+    .then((response) => response.json())
+    .then((clues) => {
+     for (let clue of clues){
+        allClues.push(clue)
+         question = clue.question
+         answer = clue.answer
+        return clueDisplay.append(question), correctAnswer.append(answer)
+        }
+      })
+    };
+
+fetchingData();
+console.log(allClues)
+
+function questionGenerator() {
+  
+}
+
+
+function verifyAnswer(answerValue) {
+    if(answerValue === answer.toLowerCase()){
+        scoreDisplay.textContent(`${score += 1}`);
+    } else {
+        let gameOver = document.createElement("h2");
+        gameOver.append("Game Over")
+        document.getElementById("play-again")
+        document.addEventListener("click", restartGame())
+    }
+  // Verify if the answer is correct
+  // use an if statement and toLoweCase
+  //display congratulatory message if correct && score +=  1 && call clue() again
+  //|| display correct answer and set timer, then display button to restart
+
+}
+
+function restartGame(){
+    window.location.reload();
+}
+//How am I gonna use the data fetched?
+// let question = document.getElementById("question")
+// question.innerHTML = fetchingData.data
+
+// function processResponse (pageResponse){
+// let randomQuestion = pageResponse.question
+//     console.log(this.question)
+// }
+
+// I have fetched the information, what is next? how do I work with the information received?
+
+//function questionGenerator(){
+//math.Random
+//}
+
+// class Jeopardy {
+//     constructor ()
+
+// }
